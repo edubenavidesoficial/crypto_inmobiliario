e1fd6c88a5c78736b15c26b08ba4454da6d6c686
henvisi-1994 <henvisi_1994@hotmail.com>
2024-03-26 21:00:13 -0500

    feat: nuevas categorias

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -16,92 +16,36 @@ class CategorySeeder extends Seeder
     {
         $datos = [
             [
-                1, 'Electrónica', '/images/categories/imagen_electronica.jpg', 'Descripción de Electrónica', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
+                1, 'Residencial', '/images/categories/house.jpg', 'Incluye propiedades diseñadas para vivienda, como casas unifamiliares, apartamentos y condominios.', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
             ],
 
             [
-                2, 'Ordenadores', '/images/categories/imagen_ordenadores.jpg', 'Descripción de Ordenadores', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
+                2, 'Comercial', '/images/categories/hotel.jpg', 'Engloba propiedades utilizadas con fines comerciales, como oficinas, tiendas minoristas, restaurantes y hoteles.', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
             ],
 
             [
-                3, 'Casa inteligente', '/images/categories/imagen_casa_inteligente.jpg', 'Descripción de Casa inteligente', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
+                3, 'Industrial', '/images/categories/factory.jpg', 'Se refiere a propiedades destinadas a actividades industriales, como almacenes, fábricas y plantas de manufactura.', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
             ],
 
             [
-                4, 'Arte y Artesanía', '/images/categories/imagen_arte_artesanía.jpg', 'Descripción de Arte y Artesanía', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
+                4, 'Terrenos', '/images/categories/terrenos.jpg', ' Incluye parcelas de tierra sin desarrollar, que pueden ser utilizadas para diversos fines, como construcción residencial, comercial o agrícola.', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
             ],
 
             [
-                5, 'Automotor', '/images/categories/imagen_automotor.jpg', 'Descripción de Automotor', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
+                5, 'Agrícola', '/images/categories/tractor.jpg', 'Se centra en propiedades utilizadas para actividades agrícolas, como granjas, ranchos y terrenos de cultivo.', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
             ],
 
             [
-                6, 'Bebé', '/images/categories/imagen_bebe.jpg', 'Descripción de Bebé', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
+                6, 'Recreacional', '/images/categories/camping.jpg', 'Engloba propiedades destinadas al ocio y la recreación, como resorts, campamentos y propiedades frente al mar.', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
             ],
 
             [
-                7, 'Belleza y cuidado personal', '/images/categories/imagen_belleza.jpg', 'Descripción de Belleza y cuidado personal', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
+                7, 'Inversión', '/images/categories/apartment-building.jpg', 'Se refiere a propiedades adquiridas con el propósito de generar ingresos a través del arrendamiento o la reventa, como edificios de apartamentos y propiedades comerciales.', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
             ],
 
             [
-                8, 'Moda femenina', '/images/categories/imagen_moda_femenina.jpg', 'Descripción de Moda femenina', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
+                8, 'Especializada', '/images/categories/gallaudet-university.jpg', 'Incluye propiedades únicas o especializadas, como hospitales, escuelas, iglesias, cárceles y estadios.', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
             ],
-
-            [
-                9, 'Moda de hombres', '/images/categories/imagen_moda_hombres.jpg', 'Descripción de Moda de hombres', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
-            ],
-
-            [
-                10, 'Moda para niñas', '/images/categories/imagen_moda_niñas.jpg', 'Descripción de Moda para niñas', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
-            ],
-
-            [
-                11, 'Moda para niños', '/images/categories/imagen_moda_niños.jpg', 'Descripción de Moda para niños', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
-            ],
-
-            [
-                12, 'Salud y Hogar', '/images/categories/imagen_salud_hogar.jpg', 'Descripción de Salud y Hogar', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
-            ],
-
-            [
-                13, 'Hogar y cocina', '/images/categories/imagen_hogar_cocina.jpg', 'Descripción de Hogar y cocina', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
-            ],
-
-            [
-                14, 'Industrial y Científico', '/images/categories/imagen_industrial_cientifico.jpg', 'Descripción de Industrial y Científico', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
-            ],
-
-            [
-                15, 'Equipaje', '/images/categories/imagen_equipaje.jpg', 'Descripción de Equipaje.jpg', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
-            ],
-
-            [
-                16, 'Películas y televisión', '/images/categories/imagen_peliculas_television.jpg', 'Descripción de Películas y televisión', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
-            ],
-
-            [
-                17, 'Suministros de mascotas', '/images/categories/imagen_mascotas.jpg', 'Descripción de Suministros de mascotas', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
-            ],
-
-            [
-                18, 'Software', '/images/categories/imagen_software.jpg', 'Descripción de Software', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
-            ],
-
-            [
-                19, 'Deportes y aire libre', '/images/categories/imagen_deportes_aire_libre.jpg', 'Descripción de Deportes y aire libre', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
-            ],
-
-            [
-                20, 'Herramientas y mejoras del hogar', '/images/categories/imagen_herramientas.jpg', 'Descripción de Herramientas y mejoras del hogar', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
-            ],
-
-            [
-                21, 'Juguetes y juegos', '/images/categories/imagen_juguetes_juegos.jpg', 'Descripción de Juguetes y juegos', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
-            ],
-
-            [
-                22, 'Juegos de vídeo', '/images/categories/imagen_juegos_video.jpg', 'Descripción de Juegos de vídeo', '2023-11-26 22:04:01', '2023-11-26 22:04:01'
-            ]
         ];
         foreach ($datos as $fila) {
             DB::insert('INSERT INTO `categories` (`id`, `name`, `image`, `description`, `created_at`, `updated_at`) VALUES (?, ?, ?, ?, ?, ?)', $fila);
