a58bc90734f52b20796a81a069d0c8d13242d1a5
Eduardo Benavides <edu3196.eb@gmail.com>
2024-05-10 10:14:19 -0500

    nuevo

--- resources/views/web/layout/header.blade.php
+++ resources/views/web/layout/header.blade.php
@@ -12,17 +12,17 @@
     <!-- Add to homescreen for Chrome on Android -->
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="application-name" content="Realty-lux">
-    <link rel="icon" sizes="512x512" href="images/icons/icon-512x512.png">
+    <link rel="icon" sizes="512x512" href="images/logo.png">
 
     <!-- Add to homescreen for Safari on iOS -->
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <meta name="apple-mobile-web-app-title" content="Realty-lux">
-    <link rel="apple-touch-icon" href="images/icons/icon-512x512.png">
+    <link rel="apple-touch-icon" href="images/logo.png">
 
     <!-- Tile for Win8 -->
     <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="msapplication-TileImage" content="images/icons/icon-512x512.png">
+    <meta name="msapplication-TileImage" content="images/logo.png">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
